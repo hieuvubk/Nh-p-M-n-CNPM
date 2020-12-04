75ffe4f5cf8573900077ff403c067b7087ad5968
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2020-12-05 00:28:00 +0700

    Add files via upload

---
+++ src/nh/p/m/n/cnpm/Connector.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nmcnpm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hp
+ */
+public class Connector {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/nmcnpm";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "";
+    
+    public Connector() {
+        
+    }
+    
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+        if(conn != null)
+            System.out.println("Connect Succesfully");
+    
+        return conn;
+    }
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connector conn = new Connector();
+        Connection a = conn.getConnection();
+    }
+}
