ebc2c95a3fbe4455d437ea96dd916865bca9bb49
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2020-12-07 16:35:27 +0700

    Update nmcnpm (1).sql

--- nmcnpm (1).sql
+++ nmcnpm (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 25, 2020 lúc 12:31 PM
+-- Thời gian đã tạo: Th12 07, 2020 lúc 10:33 AM
 -- Phiên bản máy phục vụ: 10.4.14-MariaDB
 -- Phiên bản PHP: 7.2.34
 
@@ -24,17 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `hocsinh`
+--
+
+CREATE TABLE `hocsinh` (
+  `SoHoKhau` char(10) NOT NULL,
+  `HoTen` varchar(30) NOT NULL,
+  `NgaySinh` date DEFAULT NULL,
+  `DiaChi` varchar(30) DEFAULT NULL,
+  `Truong` varchar(30) DEFAULT NULL,
+  `Lop` int(11) DEFAULT NULL,
+  `ThanhTich` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hocsinh`
+--
+
+INSERT INTO `hocsinh` (`SoHoKhau`, `HoTen`, `NgaySinh`, `DiaChi`, `Truong`, `Lop`, `ThanhTich`) VALUES
+('1', 'Nguyen Van B', '2000-05-20', 'Ha Noi', 'BK', 12, 'Gioi'),
+('1', 'Vũ Đức Hiếu', '2000-05-21', 'Vũ Xuân Thiều', 'BK', 10, 'Kha');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `hogiadinh`
 --
 
 CREATE TABLE `hogiadinh` (
   `SoHoKhau` char(1) NOT NULL,
   `HoTenChuHo` varchar(30) DEFAULT NULL,
-  `SoNha` int(11) DEFAULT NULL,
+  `SoNha` char(11) DEFAULT NULL,
   `Pho` varchar(30) DEFAULT NULL,
   `Phuong` varchar(30) DEFAULT NULL,
   `Quan` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hogiadinh`
+--
+
+INSERT INTO `hogiadinh` (`SoHoKhau`, `HoTenChuHo`, `SoNha`, `Pho`, `Phuong`, `Quan`) VALUES
+('1', 'Vũ Quang Tạo', '14a', 'Vũ Xuân Thiều', 'Sài Đồng', 'Long Biên');
 
 -- --------------------------------------------------------
 
@@ -52,6 +83,30 @@ CREATE TABLE `nguoi` (
   `NgheNghiep` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Đang đổ dữ liệu cho bảng `nguoi`
+--
+
+INSERT INTO `nguoi` (`HoTen`, `SoHoKhau`, `NgaySinh`, `DiaChi`, `QueQuan`, `NoiSinh`, `NgheNghiep`) VALUES
+('Nguyen Van A', '1', '2000-05-20', 'Ha Noi', 'Hung Yen', 'Hung Yen', 'Nha Bao'),
+('Nguyen Van B', '1', '2000-05-20', 'Ha Noi', 'Hung Yen', 'Hung Yen', 'Học Sinh'),
+('Nguyen Van C', '1', '2000-05-20', 'Ha Noi', 'Hung Yen', 'Hung Yen', 'Nha Bao'),
+('Nguyen Van D', '1', '2000-05-20', 'Ha Noi', 'Hung Yen', 'Hung Yen', 'Nha Bao'),
+('Vũ Đức Hiếu', '1', '2000-05-21', 'Vũ Xuân Thiều', 'Hưng Yên', 'Hưng Yên', 'Học Sinh');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phatqua`
+--
+
+CREATE TABLE `phatqua` (
+  `MaSuKien` char(10) NOT NULL,
+  `HoTen` varchar(30) NOT NULL,
+  `SoHoKhau` char(10) NOT NULL,
+  `MaQua` char(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
@@ -61,8 +116,8 @@ CREATE TABLE `nguoi` (
 CREATE TABLE `qua` (
   `MaQua` char(10) NOT NULL,
   `Ten` varchar(30) DEFAULT NULL,
-  `SoLuong` int(11) DEFAULT NULL,
-  `Gia` int(11) DEFAULT NULL
+  `Gia` int(11) DEFAULT NULL,
+  `SoLuong` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -76,9 +131,27 @@ CREATE TABLE `quantrivien` (
   `MatKhau` char(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sukien`
+--
+
+CREATE TABLE `sukien` (
+  `MaSuKien` char(10) NOT NULL,
+  `TenSuKien` varchar(30) DEFAULT NULL,
+  `ThoiGian` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `hocsinh`
+--
+ALTER TABLE `hocsinh`
+  ADD PRIMARY KEY (`SoHoKhau`,`HoTen`);
 
 --
 -- Chỉ mục cho bảng `hogiadinh`
@@ -94,6 +167,13 @@ ALTER TABLE `nguoi`
   ADD KEY `SoHoKhau` (`SoHoKhau`);
 
 --
+-- Chỉ mục cho bảng `phatqua`
+--
+ALTER TABLE `phatqua`
+  ADD PRIMARY KEY (`MaSuKien`,`HoTen`,`SoHoKhau`),
+  ADD KEY `SoHoKhau` (`SoHoKhau`,`HoTen`);
+
+--
 -- Chỉ mục cho bảng `qua`
 --
 ALTER TABLE `qua`
@@ -106,14 +186,33 @@ ALTER TABLE `quantrivien`
   ADD PRIMARY KEY (`TaiKhoan`);
 
 --
+-- Chỉ mục cho bảng `sukien`
+--
+ALTER TABLE `sukien`
+  ADD PRIMARY KEY (`MaSuKien`);
+
+--
 -- Các ràng buộc cho các bảng đã đổ
 --
+
+--
+-- Các ràng buộc cho bảng `hocsinh`
+--
+ALTER TABLE `hocsinh`
+  ADD CONSTRAINT `hocsinh_ibfk_1` FOREIGN KEY (`SoHoKhau`) REFERENCES `hogiadinh` (`SoHoKhau`);
 
 --
 -- Các ràng buộc cho bảng `nguoi`
 --
 ALTER TABLE `nguoi`
   ADD CONSTRAINT `SoHoKhau` FOREIGN KEY (`SoHoKhau`) REFERENCES `hogiadinh` (`SoHoKhau`);
+
+--
+-- Các ràng buộc cho bảng `phatqua`
+--
+ALTER TABLE `phatqua`
+  ADD CONSTRAINT `phatqua_ibfk_1` FOREIGN KEY (`MaSuKien`) REFERENCES `sukien` (`MaSuKien`),
+  ADD CONSTRAINT `phatqua_ibfk_2` FOREIGN KEY (`SoHoKhau`,`HoTen`) REFERENCES `nguoi` (`SoHoKhau`, `HoTen`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
