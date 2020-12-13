a93974b65c4e4165ac1e5c6e9c4d5d10a3981bc6
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2020-12-13 20:22:13 +0700

    Update HocSinh.java

--- src/nh/p/m/n/cnpm/HocSinh.java
+++ src/nh/p/m/n/cnpm/HocSinh.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nmcnpm;
+package nh.p.m.n.cnpm;;
 
 import java.sql.Connection;
 import java.sql.Date;
